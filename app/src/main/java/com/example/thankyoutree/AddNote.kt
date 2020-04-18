a238256d25e7fbd94381b336d027e09097189bb2
cult_mentality <kavitkarswanand5@gmail.com>
2020-04-18 15:25:22 +0530

    "added errorscenario and proper toast"

--- app/src/main/java/com/example/thankyoutree/AddNote.kt
+++ app/src/main/java/com/example/thankyoutree/AddNote.kt
@@ -1,32 +1,53 @@
 package com.example.thankyoutree
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.thankyoutree.model.Note
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.thankyoutree.model.Request
 import com.example.thankyoutree.retrofit.NotesApi
 import com.example.thankyoutree.retrofit.RetrofitRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_add_note.*
-import kotlinx.android.synthetic.main.activity_show_notes.*
-import kotlinx.android.synthetic.main.list_item_layout.*
+import kotlinx.android.synthetic.main.spinner_layout.view.*
 import retrofit2.Retrofit
-import java.io.IOException
-import java.io.OutputStreamWriter
 
-class AddNote : AppCompatActivity() {
+
+class AddNote : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     val retrofitRepositoryImpl: Retrofit = RetrofitRepositoryImpl().get()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+        val arrayNames= arrayListOf<String>("-","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three","one","two","three")
+        val adapter = ArrayAdapter<String>(this,R.layout.spinner_layout,arrayNames)
+        adapter.setDropDownViewResource(R.layout.spinner_layout)
+        fromSpinner.adapter = adapter
+        fromSpinner.setOnItemSelectedListener(this)
+        toSpinner.adapter = adapter
+        toSpinner.setOnItemSelectedListener(this)
+
         addNoteButton.setOnClickListener {
-            if (editNote.text.toString().isNotEmpty())
-                addNewNote(edit_from.text.toString(),editNote.text.toString(),edit_to.text.toString())
+            if (editNote.text.toString().isNotEmpty()){
+                addNewNote(fromSpinner.selectedItem.toString(),editNote.text.toString(),toSpinner.selectedItem.toString())
+            }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loading.visibility=View.GONE
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text = parent!!.getItemAtPosition(position).toString()
     }
 
     private fun addNewNote(from: String, noteData: String, to: String) {
@@ -35,12 +56,17 @@ class AddNote : AppCompatActivity() {
             .addNote(request)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                loading.visibility=View.VISIBLE
+            }
             .subscribe(
                 {
                     val intent = Intent(this, ShowNotes::class.java)
                     startActivity(intent)
                 }, {
+                    loading.visibility=View.GONE
                     Log.v("boom", it.message)
+                    Toast.makeText(this,"please check your internet connection",Toast.LENGTH_SHORT).show()
                 }
             )
 
--- app/src/main/java/com/example/thankyoutree/ShowNotes.kt
+++ app/src/main/java/com/example/thankyoutree/ShowNotes.kt
@@ -5,39 +5,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.thankyoutree.retrofit.NotesApi
 import com.example.thankyoutree.retrofit.RetrofitRepositoryImpl
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_add_note.*
 import kotlinx.android.synthetic.main.activity_show_notes.*
 import retrofit2.Retrofit
 import java.io.*
 
 class ShowNotes : AppCompatActivity() {
 
-    lateinit var notes:List<String>
-    val retrofitRepositoryImpl:Retrofit=RetrofitRepositoryImpl().get()
+    lateinit var notes: List<String>
+    val retrofitRepositoryImpl: Retrofit = RetrofitRepositoryImpl().get()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_notes)
         callApi()
     }
 
-    fun callApi(){
+    fun callApi() {
         retrofitRepositoryImpl.create(NotesApi::class.java)
             .getAllNotes()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                loadingNotes.visibility = View.VISIBLE
+            }
             .subscribe(
                 {
-                    Log.v("boom","working fine")
-                    var myAdapter:CustomArrayAdapter = CustomArrayAdapter(this,0,it)
+                    Log.v("boom", "working fine")
+                    var myAdapter: CustomArrayAdapter = CustomArrayAdapter(this, 0, it)
                     list_view_id.adapter = myAdapter
-                },{
-                    Log.v("boom",it.message)
-
+                    loadingNotes.visibility = View.GONE
+                }, {
+                    loadingNotes.visibility = View.GONE
+                    Toast.makeText(
+                        this, "please check your internet connection",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.v("boom", it.message)
                 }
             )
     }
