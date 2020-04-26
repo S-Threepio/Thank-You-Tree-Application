4497f7135d611e348527e295b3748f52bd84c395
cult_mentality <kavitkarswanand5@gmail.com>
2020-04-26 23:24:01 +0530

    back button added

--- app/src/main/java/com/example/thankyoutree/MainActivity.kt
+++ app/src/main/java/com/example/thankyoutree/MainActivity.kt
@@ -1,31 +1,19 @@
 package com.example.thankyoutree
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thankyoutree.extensions.add
-import com.example.thankyoutree.retrofit.NotesApi
-import com.example.thankyoutree.retrofit.RetrofitRepositoryImpl
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_add_note.*
-import kotlinx.android.synthetic.main.activity_landing.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_show_notes.*
 import kotlinx.android.synthetic.main.loader_layout.*
-import retrofit2.Retrofit
-import java.io.*
 
 
-class MainActivity : AppCompatActivity(),TreeBaseContract.View{
+class MainActivity : AppCompatActivity(), TreeBaseContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         add(LandingFragment())
     }
 
@@ -35,5 +23,20 @@ class MainActivity : AppCompatActivity(),TreeBaseContract.View{
 
     override fun showLoadingView() {
         loadingProgressBar.visibility = View.VISIBLE
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount != 1)
+            super.onBackPressed()
     }
 }
