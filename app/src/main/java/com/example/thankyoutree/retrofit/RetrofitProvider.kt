f7ca26e6f6a1c04b9da178470925efe516bf7744
cult_mentality <kavitkarswanand5@gmail.com>
2021-01-21 19:57:00 +0530

    added gptw api endpoints

--- app/src/main/java/com/example/thankyoutree/retrofit/RetrofitProvider.kt
+++ app/src/main/java/com/example/thankyoutree/retrofit/RetrofitProvider.kt
@@ -1,14 +1,13 @@
 package com.example.thankyoutree.retrofit
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitProvider : Provider<Retrofit> {
-    val BASE_URL = " https://z0y9tg6nl3.execute-api.ap-south-1.amazonaws.com/"
+    val BASE_URL = " https://7z0ng7ffm2.execute-api.ap-south-1.amazonaws.com/"
     val gson = GsonConverterFactory.create()
 
     private val logging: HttpLoggingInterceptor =
