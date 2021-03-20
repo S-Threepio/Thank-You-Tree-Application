8bdd2d3aed64167671c251b38b9c39a0dab9a7c0
cult_mentality <kavitkarswanand5@gmail.com>
2021-03-20 13:20:30 +0530

    Revert "added filters and dialog fragments"
    This reverts commit ac05952b

--- app/src/main/java/com/example/thankyoutree/views/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/thankyoutree/views/notes/NotesViewModel.kt
@@ -2,9 +2,7 @@ package com.example.thankyoutree.views.notes
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.thankyoutree.model.NamesOfEmployees
 import com.example.thankyoutree.model.Notes
-import com.example.thankyoutree.model.liveDataReponses.NamesResponse
 import com.example.thankyoutree.model.liveDataReponses.NotesResponse
 import com.example.thankyoutree.model.liveDataReponses.Status
 import com.example.thankyoutree.retrofit.NotesApi
@@ -19,17 +17,11 @@ class NotesViewModel : ViewModel() {
     private val compositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
 
     var notesLiveData = MutableLiveData<NotesResponse>()
-    var namesLiveData = MutableLiveData<NamesResponse>()
-    var displayLiveData = MutableLiveData<NotesResponse>()
 
     val viewModelJob = Job()
     val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     init {
-        callNotesApi()
-    }
-
-    fun callNotesApi() {
         uiScope.launch {
             notesLiveData.value = loading()
             try {
@@ -45,7 +37,6 @@ class NotesViewModel : ViewModel() {
             retrofitRepositoryImpl.create(NotesApi::class.java)
                 .getAllNotes().await().apply {
                     notesLiveData.postValue(success(Notes(this)))
-                    displayLiveData.postValue(success(Notes(this)))
                 }
         }
     }
@@ -65,46 +56,6 @@ class NotesViewModel : ViewModel() {
 
     fun error(error: Throwable?): NotesResponse? {
         return NotesResponse(
-            Status.ERROR,
-            error = error
-        )
-    }
-
-    fun callNamesApi() {
-        uiScope.launch {
-            namesLiveData.value = namesLoading()
-            try {
-                fetchNames()
-            } catch (e: Throwable) {
-                namesLiveData.value = namesError(e)
-            }
-        }
-    }
-
-    private suspend fun fetchNames() {
-        withContext(Dispatchers.IO) {
-            retrofitRepositoryImpl.create(NotesApi::class.java)
-                .getListOfNames().await().apply {
-                    namesLiveData.postValue(namesSuccess(this))
-                }
-        }
-    }
-
-    fun namesLoading(): NamesResponse? {
-        return NamesResponse(
-            Status.LOADING
-        )
-    }
-
-    fun namesSuccess(data: NamesOfEmployees): NamesResponse? {
-        return NamesResponse(
-            Status.SUCCESS,
-            data
-        )
-    }
-
-    fun namesError(error: Throwable?): NamesResponse? {
-        return NamesResponse(
             Status.ERROR,
             error = error
         )
